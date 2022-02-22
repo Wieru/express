81a1ab9f33b731351c70cb9e9df23e1e839438f5
Szymon Wierucki <szymon.wierucki@gmail.com>
2022-02-22 14:39:37 +0100

    Add code for 27.3 module

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const path = require('path');
+const hbs = require('express-handlebars');
+app.engine('.hbs', hbs());
+app.set('view engine', '.hbs');
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.show = (name) => {
+    res.sendFile(path.join(__dirname, `/views/${name}`));
+  };
+  next();
+});
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/hello/:name', (req, res) => {
+  res.render('hello', { layout: false, name: req.params.name });
+});
+
+app.get('/', (req, res) => {
+  res.show('index.html');
+});
+
+app.get('/about', (req, res) => {
+  res.show('about.html');
+});
+
+app.get('/contact', (req, res) => {
+  res.show('contact.html');
+});
+
+app.get('/info', (req, res) => {
+  res.show('info.html');
+});
+
+app.get('/history', (req, res) => {
+  res.show('history.html');
+});
+
+app.use((req, res) => {
+  res.status(404).send('404 not found...');
+})
+
+app.listen(8000, () => {
+  console.log('Server is running on port: 8000');
+});
